8ccc1760099b92a834b98c65f84706d861a0301f
Eduardo Azevedo <eduardoazvd17@gmail.com>
2025-02-06 18:22:43 -0300

    Making api call using http

---
+++ lib/src/core/utils/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+  Constants._();
+
+  static const String kMockApi = 'https://www.jsonkeeper.com/b/1IXK';
+}
--- lib/src/features/home/data/datasources/home_datasource.dart
+++ lib/src/features/home/data/datasources/home_datasource.dart
@@ -1,10 +1,20 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import '../../../../core/core.dart';
+
 abstract class IHomeDatasource {
   Future<Map<String, dynamic>> getMotelList();
 }
 
 class HomeDatasourceImpl implements IHomeDatasource {
+  final http.Client _httpClient;
+  HomeDatasourceImpl({required http.Client httpClient})
+      : _httpClient = httpClient;
+
   @override
   Future<Map<String, dynamic>> getMotelList() async {
-    throw UnimplementedError();
+    final response = await _httpClient.get(Uri.parse(Constants.kMockApi));
+    return Map<String, dynamic>.from(jsonDecode(response.body));
   }
 }
